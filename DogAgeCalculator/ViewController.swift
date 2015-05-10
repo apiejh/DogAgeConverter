8b22e3220726c09f47ce0444d26f4d26f33495d8
Gaper <gaperlinski@MacBook-Pro-Grzegorz.local>
2015-05-10 15:17:02 +0200

    dog age calculator complete

--- DogAgeCalculator/ViewController.swift
+++ DogAgeCalculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertToDogYearsPressed(sender: UIButton) {
+        let conversionConstant =  7
+        if enterHumanYearsTextField.text != "" {
+            let ageFromTextField = enterHumanYearsTextField.text.toInt()!
+            if enterNameTextField.text != "" {
+                resultLabel.text = "\(enterNameTextField.text) is \(ageFromTextField * conversionConstant) years old in human years."
+            } else {
+                resultLabel.text = "Your dog is \(ageFromTextField * conversionConstant) years old in human years."
+            }
+        } else {
+            resultLabel.text = "You need to write the age of your dog."
+        }
+        enterHumanYearsTextField.text = ""
+        enterNameTextField.text = ""
+        
+        enterHumanYearsTextField.resignFirstResponder()
+        enterNameTextField.resignFirstResponder()
+        
+        resultLabel.hidden = false
+    }
 }
 
