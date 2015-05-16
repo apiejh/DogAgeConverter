6921e9b62cd65f3a604b78ea6598330eb1ad9a26
gaperlinski <apiejh@gmail.com>
2015-05-17 01:01:35 +0200

    converted to oop

---
+++ DogAgeCalculator/Being.swift
@@ -0,0 +1,9 @@
+//
+//  Being.swift
+//  DogAgeCalculator
+//
+//  Created by Grzegorz Aperliński on 16.05.2015.
+//  Copyright (c) 2015 Grzegorz Aperliński. All rights reserved.
+//
+
+import Foundation
